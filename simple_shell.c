517f3c6650e912a8afce3792eb67697f34defe6f
Errafai <errafaiachraf1923@gmail.com>
2023-10-10 19:16:11 +0000

    task

--- simple_shell.c
+++ simple_shell.c
@@ -2,19 +2,21 @@
 
 char **spliter(char *command)
 {
+	char *token;
 	char **array;
-	int i = 1;
+	int i = 0;
 
 	array = malloc(sizeof(char*) * 1024);
 	if (array == NULL)
 	{
 		perror("malloc faild");
-		exit(1);
+		return (NULL);
 	}
-	array[0] = strtok(command, " \n\t");
+	token = strtok(command, " \n\t");
 	while (command)
 	{
 		array[i] = strtok(NULL, " \n\t");
+		token = strtok(NULL, " \n\t");
 		i++;
 	}
 	array[i] = NULL;
@@ -26,7 +28,7 @@ int main(int ac, char *av[], char **env)
 	int status, i = 0, n_char;
 	char *buffer = NULL;
 	size_t buffer_size = 0;
-	char *token;
+	char *cmd;
 	char **full_command;
 	pid_t pid;
 
@@ -34,17 +36,27 @@ int main(int ac, char *av[], char **env)
 	{
 		write(1, "$ ", 2);
 		n_char = _getline(&buffer, &buffer_size, 0);
-		token = _command(buffer);
-		full_command = spliter(token);
+		if (n_char == -1)
+		{
+			write(1, "\n", 1);
+			exit(1);
+		}
+		full_command = spliter(buffer);
+		if (strcmp(full_command[0], "exit") == 0)
+			exit(0);
+
 		pid = fork();
 		if (pid == 0)
 		{
-			if (execve(full_command[0], full_command, env) == -1)
-				perror("execve");
+			cmd = _command(full_command[0]);
+			if (cmd)
+				execve(cmd, full_command, env);
+			else
+				perror("command not found");
+			exit(0);
 		}
 		else
 			wait(&status);
-		i = 0;
-		free(full_command);
 	}
+	return (0);
 }
