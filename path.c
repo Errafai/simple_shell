cc74b3ba7b2cce5501d3d62bd337059b60e5f934
Errafai <errafaiachraf1923@gmail.com>
2023-10-10 19:20:10 +0000

    task

--- path.c
+++ path.c
@@ -2,23 +2,22 @@
 
 char *_command(const char *cmd)
 {
-	int n;
 	char *all_paths  = getenv("PATH");
 	char *token;
 	char *path;
 	struct stat st;
 
 	token = strtok(all_paths, ":");
-	n = sizeof(token) + sizeof(cmd) + 2;
 	while (token != NULL)
 	{
-		path = malloc(n);
+		path = malloc(sizeof(token) + sizeof(cmd) + 2);
 		strcpy(path, token);
 		strcat(path, "/");
 		strcat(path, cmd);
 		if (stat(path, &st) == 0)
 			return(path);
 		token = strtok(NULL, ":");
+		free(path);
 	}
 	return (NULL);
 }
