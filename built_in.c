9dd9f6d09e784a8ebd805eebd3b91ee34dab474c
Errafai <errafaiachraf1923@gmail.com>
2023-10-18 01:25:36 -0700

    task5

--- built_in.c
+++ built_in.c
@@ -54,12 +54,11 @@ void exit_shell(char **command, int *status)
 void print_env(char **command, int *status)
 {
 	int i;
-
+	(void) command;
 	for (i = 0; environ[i]; i++)
 	{
 		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
 		write(STDOUT_FILENO, "\n", 1);
 	}
-	free_array(command);
 	*status = 0;
 }
